423ba3c34a498237342aef9456cfd870c187ba53
Ted Wong <ted@pacificanalytics.com.au>
2019-02-19 13:36:31 +1100

    Switched to python3

--- chessgames.py
+++ chessgames.py
@@ -1,19 +1,21 @@
 import os
 import sys
 import urllib
-import urlparse
 from lxml import html
+from urllib.parse import urlsplit
+from urllib.parse import parse_qsl
+from urllib.request import urlopen
 from multiprocessing.dummy import Pool as ThreadPool 
 
 def readGameLinks(url, cur=1):
     links = []
-    page = html.fromstring(urllib.urlopen(url).read())
+    page = html.fromstring(urlopen(url).read())
     hasNext = False    
     nextURL = None
     for link in page.xpath("//a"):
         if link.get("href") != None:
             if 'chessgame?gid' in link.get("href"):
-                parsed = dict(urlparse.parse_qsl(urlparse.urlsplit(link.get("href")).query))
+                parsed = dict(parse_qsl(urlsplit(link.get("href")).query))
                 links.append(parsed['gid'])
             elif 'page=' in link.get("href") and hasNext == False:
                 page = int(link.get('href').split('page=')[1].split('&')[0])
@@ -25,16 +27,14 @@ def readGameLinks(url, cur=1):
     return links
 
 def readPGN(url):
-    return urllib.urlopen(url).read()
+    return urlopen(url).read().decode("utf-8")
 
 def readGame(game):
     url = 'http://www.chessgames.com/perl/nph-chesspgn?gid={0}&text=1'.format(game)
     return readPGN(url) + '\n'
 
 # http://www.chessgames.com/perl/chessplayer?pid=24694
-url = sys.argv[1]
-
-toks = url.split('?')
+toks = sys.argv[1].split('?')
 
 # http://www.chessgames.com/perl/chess.pl?page=1&pid=24694
 url = 'http://www.chessgames.com/perl/chess.pl?page=1&' + toks[1]
@@ -42,7 +42,4 @@ url = 'http://www.chessgames.com/perl/chess.pl?page=1&' + toks[1]
 # Parse all games for the player
 games = list(reversed(readGameLinks(url)))
 
-# Read the games
-results = [readGame(i) for i in games]
-
-print ''.join(results)
+print(''.join([readGame(i) for i in games]))
